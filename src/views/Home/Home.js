251b1ce3e2babd79fa9c3b4b06b949f79236ec88
Priti Kothe <pritikothe15@gmail.com>
2023-01-07 00:24:56 +0530

    feat:add note

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,56 +1,73 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
-import "./Home.css"
-import Navbar from "./../../components/Navbar/Navbar"
-import Note from "./../../components/Note/Note"
+import "./Home.css";
+import Note from "./../../components/Note/Note";
 
 function Home() {
-    const [notes, setNotes] =useState([
-        {
-            Title: "Note 1",
-            Content :"This is Note 1 "
-        },
-        {
-            Title: "Note 2",
-            Content :"This is Note 2 "
-        },
-        {
-            Title: "Note 3",
-            Content :"This is Note 3 "
-        },
-        {
-            Title: "Note 4",
-            Content :"This is Note 4 "
-        },
-        // {
-        //     Title: "Note 5",
-        //     Content :"This is Note 5 "
-        // },
-        // {
-        //     Title: "Note 6",
-        //     Content :"This is Note 6 "
-        // },
+  const [notes, setNotes] = useState([
+    {
+      title: "Note ",
+      content: "You can add more notes to this list ",
+    },
+   
+  ]);
 
-    ])
+  const [title,setTitle]=useState("")
+  const [content,setContent]=useState("")
+
+  function addNote(){
+    const newNote={
+        "title":title,
+        "content":content
+    }
+    // const temp =notes;
+    // temp.push(newNote)
+    // setNotes(temp);
+
+   setNotes([...notes,newNote])
+
+   alert('note added successfully!')
+
+   setTitle("")
+   setContent("")
+  }
+
   return (
     <div>
-        <Navbar/> 
-        <div className='app-title-container'> 
-        <h1 className='app-title'> My Notes </h1>
+      <div className="app-title-container">
+        <h1 className="app-title"> My Notes </h1>
+      </div>
+
+      <div className="row">
+        <div className="col-md-6">
+          <div className="notes-container">
+            {notes.map((note, index) => {
+              return <Note Title={note.title} Content={note.content} />;
+            })}
+          </div>
         </div>
 
-        <div className='notes-container'> 
-           {
-            notes.map((note,index)=>{
-              return(
-                <Note Title = {note.Title} Content= {note.Content} />
-              )
-               
-            })
-           }
+        <div className="col-md-6">
+            <div className="note-editor-container">
+                <h3 className="text-center"> Add Note</h3>
+                <form>
+                    <div>
+                        <input type="text" value={title} onChange={(e)=>{setTitle(e.target.value)}} className="form-control mt-4"placeholder="Note-Title"/>
+                    </div>
+
+                    <div>
+                        <input type="text" value={content} onChange={(e)=>{setContent(e.target.value)}}className="form-control mt-4"placeholder="Note-Description"/>
+                    </div>
+
+                    <div className="d-flex justify-content-center"> 
+                        <button type="button" className="btn btn-primary mt-4 "onClick={addNote} >Add Note </button>
+                    </div>
+                </form>
+            </div> 
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
